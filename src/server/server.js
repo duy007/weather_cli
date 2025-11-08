26ebc43737b1a181dfb7342a2503e306b02a26f8
Duy Nguyen <duyhoangnguyen2000@gmail.com>
2025-11-07 16:49:54 -0800

    init server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,2 +1,26 @@
+import http from 'node:http'
+import fs from 'node:fs/promises'
 
+export const createServer = () => {
+    return http.createServer(async (req, res) => {
 
+    })
+}
+
+export const start = ( port) => {
+    const server = createServer()
+    server.listen(port, () => {
+        const address = `http://localhost:${port}`
+        console.log(`Server is listening on ${address}`);
+        open(address)
+    });
+}
+// POST: SEARCH <LAT> <LOT>
+
+// GET: GET <CITY> <STATE>
+
+// GET: ZONES
+
+// GET: ALLFORECAST
+
+// POST: CLEAN
